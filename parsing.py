3fd58ad599e0fb3f1576ddcbe9da596c8861d7b1
Elmir <elmir.iskhakov1337@gmail.com>
2022-04-08 21:34:09 +0300

    Add comments

--- parsing.py
+++ parsing.py
@@ -1,41 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
-import json
+import json   #импортирование необходимых библиотек
 
-# url = "https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya"
+url = "https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya"  #ссылка на сайт Бонча
 
-# headers = {
-#     "Accept": "*/*",
-#     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0"
-# }
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0"
+}
 
-# req = requests.get(url, headers = headers)
-
-
-# src = req.text
-
-# with open("index.html", "w") as file:
-#     file.write(src)
+req = requests.get(url, headers = headers) #создаем get запрос
 
 
-# with open("index.html", "r") as file:
-#     src = file.read()
+src = req.text #получаем html текст
 
+with open("index.html", "w") as file: #записываем html страницу в отдельный файл 
+    file.write(src)
 
+soup = BeautifulSoup(src) #"прогоняем через парсер" 
 
-# soup = BeautifulSoup(src)
+all_groups_href = soup.find_all(class_ = "vt256") #находим ссылки на расписание каждой группы 
 
-# all_groups_href = soup.find_all(class_ = "vt256")
+all_schedules = {} #создаем словарь
 
-# all_schedules = {}
+for item in all_groups_href: #заполняем словарь
+    group_name = item.text.strip()
+    group_schedule_href = "https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya" + item.get("href").strip()
+    print(group_name, group_schedule_href)
 
-# for item in all_groups_href:
-#     group_name = item.text.strip()
-#     group_schedule_href = "https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya" + item.get("href").strip()
-#     print(group_name, group_schedule_href)
+    all_schedules[group_name] = group_schedule_href
 
-#     all_schedules[group_name] = group_schedule_href
-
-# with open("data.json", "w") as file:
-#     json.dump(all_schedules, file, indent=4, ensure_ascii = False)
+with open("data.json", "w") as file:  #записываем словарь в json файл
+    json.dump(all_schedules, file, indent=4, ensure_ascii = False)
  
